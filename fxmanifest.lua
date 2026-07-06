fefa63dbba4f05985caee74cf8b58c81e8b1ff25
JonathaF0 <110953872+JonathaF0@users.noreply.github.com>
2026-07-05 21:38:14 -0400

    Update fxmanifest with new version and scripts
    Updated metadata and added new scripts for 911 and reader functionalities.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'CDECAD'
-description 'CDECAD unified resource: tablet + duty + civ + 911 + wraith + ers'
-author 'CDECAD'
-version '1.0.0'
+description 'CDECAD with SA lore departments (LSPD, LCSO, SASP, BCSO, LSFD, BCFD).'
+author 'CDE Inc'
+version '4.0.0'
 
 lua54 'yes'
 
@@ -18,10 +18,10 @@ server_scripts {
     'tablet/server_main.lua',
     'duty/server.lua',
     'duty/diagnostics.lua',
-    'panic/server.lua',
     '@oxmysql/lib/MySQL.lua',
     'civ/server_main.lua',
     '911/server.lua',
+    '911/alpr_server.lua',
     'wraith/server.lua',
     'ers/server.lua',
 }
@@ -30,11 +30,11 @@ client_scripts {
     'tablet/client_main.lua',
     'duty/client.lua',
     'duty/diagnostics.lua',
-    'panic/client.lua',
     'civ/client_main.lua',
     'civ/client_nui.lua',
     '911/client.lua',
     '911/npc.lua',
+    '911/alpr_client.lua',
     'wraith/client.lua',
     'ers/client.lua',
 }
@@ -50,6 +50,7 @@ files {
     'html/civ/style.css',
     'html/civ/script.js',
     'html/wraith/index.html',
+    'html/reader/index.html',
 }
 
 dependencies {
